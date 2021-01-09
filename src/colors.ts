df236a57b3ff51676bb99e7d55ab56e740252498
Nick Artman <nicolasartman@gmail.com>
2021-01-09 02:14:15 -0800

    Build out first site direction draft

--- src/colors.ts
+++ src/colors.ts
@@ -1,5 +1,10 @@
+// Core colors
 export const red = "#CA000A"
 export const darkRed = "#7D0006"
 export const purple = "#5A508B"
 export const almostWhite = "#E7E7E7"
-export const purpleRedGradient = `linear-gradient(315deg, ${purple} -20%, ${red} 120%)`
+export const white = "#FFFFFF"
+export const transparent = "rgba(0, 0, 0, 0)"
+
+// Contextual
+export const heroBannerBackground = `linear-gradient(315deg, ${purple} -20%, ${red} 120%)`
