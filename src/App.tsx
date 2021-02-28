1b229514adada3c21aaf99e6be3ecd0521d5b11a
Nick Artman <nicolasartman@gmail.com>
2021-02-28 02:48:30 -0800

    Switch to system font stack

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,9 @@ const theme = deepMerge<ThemeType, ThemeType>(base, {
       accent: red,
     },
     font: {
-      family: "Roboto",
+      family:
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, ' +
+        'Ubuntu, Cantarell,"Helvetica Neue",sans-serif;',
       size: "18px",
     },
     breakpoints: {
