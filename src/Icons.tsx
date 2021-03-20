4849eab91967a89acd11565c3174e658495a1cd3
Nick Artman <nicolasartman@gmail.com>
2021-03-20 03:05:15 -0700

    Add Icons file and some initial icons

---
+++ src/Icons.tsx
@@ -0,0 +1,182 @@
+export const BrainIcon = () => (
+  <svg width={50} height={50}>
+    <defs>
+      <linearGradient x1="47.593%" y1="-2.552%" x2="47.593%" y2="100%" id="brain_a">
+        <stop stopColor="#FFABAB" offset="0%" />
+        <stop stopColor="#FF6767" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="47.593%" y1="-2.552%" x2="47.593%" y2="100%" id="brain_b">
+        <stop stopColor="#FFABAB" offset="0%" />
+        <stop stopColor="#FF6767" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="25.158%" y1="1.915%" x2="58.276%" y2="79.877%" id="brain_c">
+        <stop stopColor="#F77" offset="0%" />
+        <stop stopColor="#FF3636" offset="100%" />
+      </linearGradient>
+    </defs>
+    <g fill="none" fillRule="evenodd">
+      <path d="M0 0h50v50H0z" />
+      <path
+        d="M41 27c3.333 2.382 5 6.049 5 11l.043 5.002L42 43c.012-6.248-1.32-9.582-4-10-2.68-.418-1.68-2.418 3-6z"
+        fill="url(#brain_a)"
+      />
+      <path
+        d="M43.923 18.77C46.25 19.236 48 21.251 48 23.666c0 2.761-2.288 5-5.111 5a5.204 5.204 0 0 1-1.724-.292c-.283 3.89-3.599 6.958-7.646 6.958a7.802 7.802 0 0 1-2.67-.467A6.866 6.866 0 0 1 25.852 37c-2.747 0-5.114-1.59-6.193-3.88a5.181 5.181 0 0 1-2.326.547c-1.81 0-3.4-.92-4.308-2.309A5.176 5.176 0 0 1 10.52 32c-2.823 0-5.112-2.239-5.112-5 0-.65.127-1.272.358-1.842C3.595 24.58 2 22.64 2 20.333c0-2.18 1.426-4.034 3.415-4.718a4.97 4.97 0 0 1-.008-.282c0-2.761 2.289-5 5.112-5 0-2.761 2.288-5 5.11-5 .665 0 1.3.125 1.883.35C18.103 3.561 20.087 2 22.444 2c1.81 0 3.4.92 4.308 2.308a5.176 5.176 0 0 1 2.507-.641c2.357 0 4.342 1.56 4.932 3.683A5.197 5.197 0 0 1 36.074 7c2.16 0 4.006 1.31 4.755 3.162a5.502 5.502 0 0 1 3.094 8.607z"
+        fill="url(#brain_b)"
+      />
+      <path
+        d="M28.163 11.456a4.9 4.9 0 0 1-.745.649c-.864.611-1.724.852-2.353.893a1 1 0 0 1-.13-1.996 3.102 3.102 0 0 0 1.328-.53 2.9 2.9 0 0 0 .641-.621c-.283-.436-.55-.905-.799-1.405a1 1 0 0 1 1.79-.892c1.306 2.62 3.051 4.074 5.276 4.46a1 1 0 1 1-.342 1.971c-1.809-.314-3.367-1.165-4.666-2.529zm7.78 12.027c1.404 1.226 2.131 2.75 2.131 4.517a1 1 0 0 1-2 0c0-1.101-.412-2.038-1.277-2.857a7.76 7.76 0 0 1-2.402 1.759 1 1 0 1 1-.864-1.804c2.198-1.054 3.347-3.046 3.47-6.138a1 1 0 1 1 1.998.08c-.068 1.714-.42 3.2-1.055 4.443zm-20.26-1.727c1.574-1.853 3.82-2.433 6.567-1.724a1 1 0 1 1-.5 1.936c-1.933-.499-3.35-.196-4.4.924.492.423.967.915 1.425 1.476a1 1 0 1 1-1.55 1.264c-1.584-1.942-3.277-2.788-5.144-2.635a1 1 0 1 1-.162-1.994 6.868 6.868 0 0 1 3.765.753zm1.359-13.47a1 1 0 1 1 1.916-.572c.756 2.536.368 4.866-1.16 6.889a1 1 0 0 1-1.596-1.206c1.139-1.507 1.417-3.176.84-5.111zm7.988 19.47a1 1 0 1 1 1.94.488c-.607 2.416-2.211 4.02-4.7 4.719a1 1 0 1 1-.54-1.926c1.808-.508 2.87-1.57 3.3-3.28zM4.307 15.721a1 1 0 1 1 1.386-1.442c1.42 1.364 2.795 1.914 4.174 1.73a1 1 0 0 1 .266 1.982c-2.036.272-3.994-.511-5.826-2.27zm6.863 12.837a1 1 0 1 1 1.66-1.116C13.603 28.592 14 29.782 14 31a1 1 0 0 1-2 0c0-.8-.27-1.609-.83-2.442zm17.87 4.723a1 1 0 0 1 1.92-.562c.191.656.467 1.207.826 1.662a1 1 0 1 1-1.572 1.238c-.522-.663-.913-1.445-1.174-2.338z"
+        fill="url(#brain_c)"
+      />
+    </g>
+  </svg>
+)
+
+export const MagnifyingGlassAndPaperIcon = () => (
+  <svg width={50} height={50}>
+    <defs>
+      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="magnifying_glass_and_paper_a">
+        <stop stopColor="#E7EBF8" offset="0%" />
+        <stop stopColor="#D1D9EB" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="magnifying_glass_and_paper_b">
+        <stop stopColor="#FFCB00" offset="0%" />
+        <stop stopColor="#FF8E1F" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="2.329%" x2="50%" y2="96.576%" id="magnifying_glass_and_paper_c">
+        <stop stopColor="#525879" offset="0%" />
+        <stop stopColor="#17214C" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="53.481%" y1="0%" x2="53.481%" y2="100%" id="magnifying_glass_and_paper_d">
+        <stop stopColor="#499CFF" offset="0%" />
+        <stop stopColor="#7E41FF" offset="100%" />
+      </linearGradient>
+      <linearGradient
+        x1="52.85%"
+        y1="0%"
+        x2="54.007%"
+        y2="105.293%"
+        id="magnifying_glass_and_paper_e"
+      >
+        <stop stopColor="#6BAFFF" offset="0%" />
+        <stop stopColor="#7E41FF" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="f">
+        <stop stopColor="#FFF0B4" offset="0%" />
+        <stop stopColor="#FFD0A3" offset="100%" />
+      </linearGradient>
+      <linearGradient
+        x1="48.979%"
+        y1="20.684%"
+        x2="48.979%"
+        y2="104.349%"
+        id="magnifying_glass_and_paper_g"
+      >
+        <stop stopColor="#62698F" offset="0%" />
+        <stop stopColor="#475078" offset="100%" />
+      </linearGradient>
+    </defs>
+    <g fill="none" fillRule="evenodd">
+      <path d="M0 0h50v50H0z" />
+      <path
+        d="M3 2h34a1 1 0 0 1 1 1v44a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"
+        fill="url(#magnifying_glass_and_paper_a)"
+      />
+      <path
+        d="M7 6h13a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1zm0 16h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1zm0-8h26a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1z"
+        fill="url(#magnifying_glass_and_paper_b)"
+      />
+      <path
+        fill="url(#magnifying_glass_and_paper_c)"
+        d="M36.168 35.386l1.554-1.258 4.604 5.684-1.555 1.259z"
+      />
+      <circle fill="url(#magnifying_glass_and_paper_d)" cx={30.702} cy={27.048} r={12} />
+      <path
+        d="M37.845 36.691C32.71 40.5 25.438 39.615 21.377 34.6c-4.115-5.081-3.397-12.503 1.567-16.707l14.9 18.798z"
+        fill="url(#magnifying_glass_and_paper_e)"
+      />
+      <circle fill="#F1F4FF" cx={30.702} cy={27.048} r={9} />
+      <path
+        d="M21.764 26a8.994 8.994 0 0 1 1.485-4H27a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-5.236z"
+        fill="url(#magnifying_glass_and_paper_f)"
+      />
+      <path d="M38 21.78v10.54a9.005 9.005 0 0 0 0-10.54z" fill="#FFF" />
+      <path
+        d="M39.796 41.456a2 2 0 1 1 3.109-2.518l4.603 5.685a2 2 0 0 1-3.109 2.517l-4.603-5.684z"
+        fill="url(#magnifying_glass_and_paper_g)"
+      />
+    </g>
+  </svg>
+)
+
+export const PodiumIcon = () => (
+  <svg width={50} height={50}>
+    <defs>
+      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="podium_a">
+        <stop stopColor="#FFCB00" offset="0%" />
+        <stop stopColor="#FF7E00" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="-22.776%" x2="50%" y2="100%" id="podium_b">
+        <stop stopColor="#FFCB00" offset="0%" />
+        <stop stopColor="#FF7E00" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="podium_c">
+        <stop stopColor="#FFD52F" offset="0%" />
+        <stop stopColor="#FF952D" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="podium_d">
+        <stop stopColor="#FFCB00" offset="0%" />
+        <stop stopColor="#FF7E00" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="48.979%" y1="20.684%" x2="48.979%" y2="104.349%" id="podium_e">
+        <stop stopColor="#62698F" offset="0%" />
+        <stop stopColor="#475078" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="-15.026%" x2="50%" y2="94.224%" id="podium_f">
+        <stop stopColor="#FF9696" offset="0%" />
+        <stop stopColor="#FF2D2D" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="-15.026%" x2="50%" y2="94.224%" id="podium_g">
+        <stop stopColor="#FF9292" offset="0%" />
+        <stop stopColor="#FC0101" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="48.979%" y1="20.684%" x2="48.979%" y2="104.349%" id="podium_h">
+        <stop stopColor="#62698F" offset="0%" />
+        <stop stopColor="#475078" offset="100%" />
+      </linearGradient>
+      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="podium_i">
+        <stop stopColor="#FFCB00" offset="0%" />
+        <stop stopColor="#FF7E00" offset="100%" />
+      </linearGradient>
+    </defs>
+    <g fill="none" fillRule="evenodd">
+      <path d="M0 0h50v50H0z" />
+      <g transform="translate(7 2)">
+        <path fill="url(#podium_a)" d="M1 16l3 3v-4z" />
+        <path fill="url(#podium_b)" d="M35 16l-3 3v-4z" />
+        <path
+          d="M3.263 12h29.474a1 1 0 0 1 .974 1.229l-7.53 32a1 1 0 0 1-.973.771H10.792a1 1 0 0 1-.973-.771l-7.53-32A1 1 0 0 1 3.263 12z"
+          fill="url(#podium_c)"
+        />
+        <path
+          d="M19 12h13.737a1 1 0 0 1 .974 1.229l-7.53 32a1 1 0 0 1-.973.771H19a1 1 0 0 1-1-1V13a1 1 0 0 1 1-1z"
+          fill="url(#podium_d)"
+        />
+        <path
+          d="M23.22 3.625a1 1 0 1 1 1.56-1.25l4 5A1 1 0 0 1 29 8v6a1 1 0 0 1-2 0V8.35l-3.78-4.725z"
+          fill="url(#podium_e)"
+        />
+        <path
+          d="M1 10h34a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1z"
+          fill="url(#podium_f)"
+        />
+        <path d="M18 10h17a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H18v-6z" fill="url(#podium_g)" />
+        <path
+          d="M8 40h20a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1z"
+          fill="url(#podium_h)"
+        />
+        <circle fill="url(#podium_i)" cx={24} cy={3} r={3} />
+      </g>
+    </g>
+  </svg>
+)
