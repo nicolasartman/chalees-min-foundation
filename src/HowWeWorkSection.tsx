7cf0c998ad5b79c9b898eedbc6c51aba3eab87d7
Nick Artman <nicolasartman@gmail.com>
2021-03-22 01:42:41 -0700

    Update design of How We work Section

--- src/HowWeWorkSection.tsx
+++ src/HowWeWorkSection.tsx
@@ -9,10 +9,15 @@ type HowWeWorkCardProps = {
   text: string
   pictureSource: string
   icon: ReactNode
+  isMobileLayout: boolean
 }
 
 const HowWeWorkCard = (props: HowWeWorkCardProps) => (
-  <Box direction="row" fill="horizontal">
+  <Box
+    direction={props.isMobileLayout ? "row" : "column"}
+    gap={props.isMobileLayout ? "none" : "small"}
+    fill="horizontal"
+  >
     <Box direction="row" align="start" flex="grow" pad={{ right: "20px" }}>
       {props.icon}
     </Box>
@@ -27,7 +32,7 @@ const HowWeWorkCard = (props: HowWeWorkCardProps) => (
 
 const HowWeWorkSection = (props: BaseSectionProps) => (
   <SectionContainer flex="grow">
-    <Box pad={{ horizontal: "40px" }}>
+    <Box pad={{ horizontal: "40px" }} style={{ maxWidth: 1600 }} alignSelf="center">
       <Box direction="column" fill="horizontal" align="start">
         <Heading level={2} margin="0">
           How we work
@@ -35,13 +40,19 @@ const HowWeWorkSection = (props: BaseSectionProps) => (
         <Box fill>After our teachers have taught a chapter from the textbook, our...</Box>
       </Box>
       <Box height="20px" />
-      <Box direction="column" gap="40px" pad={{ vertical: "medium" }} align="start">
+      <Box
+        direction={props.isMobileLayout ? "column" : "row"}
+        gap="40px"
+        pad={{ vertical: "medium" }}
+        align="start"
+      >
         <Box fill="horizontal">
           <HowWeWorkCard
             title="Students ask questions"
             text="on a topic they just finished reading from the book; a question that is interesting to them."
             pictureSource="https://placekitten.com/50/50"
             icon={<BrainIcon />}
+            isMobileLayout={props.isMobileLayout}
           />
         </Box>
         <Box fill="horizontal">
@@ -50,6 +61,7 @@ const HowWeWorkSection = (props: BaseSectionProps) => (
             text="by asking an older sibling, or by reading a book from the library, or by searching the Internet."
             pictureSource="https://placekitten.com/50/50"
             icon={<MagnifyingGlassAndPaperIcon />}
+            isMobileLayout={props.isMobileLayout}
           />
         </Box>
         <Box fill="horizontal">
@@ -58,6 +70,7 @@ const HowWeWorkSection = (props: BaseSectionProps) => (
             text="by coming in front of class and teaching their peers, or by making short videos."
             pictureSource="https://placekitten.com/50/50"
             icon={<PodiumIcon />}
+            isMobileLayout={props.isMobileLayout}
           />
         </Box>
       </Box>
