3a496c1dbeb418c1a457b3d10fb4b432d2a54c81
Nick Artman <nicolasartman@gmail.com>
2021-04-03 21:51:58 -0700

    Update partners section design to use carousel; update SCT logo

--- src/OurPartnersSection.tsx
+++ src/OurPartnersSection.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, Image, Text } from "grommet"
+import { useKeenSlider } from "keen-slider/react"
 import { BaseSectionProps } from "./BaseSectionProps"
 import bahujanLogoUrl from "./images/partners/bahujan-samajik-trust-logo.jpg"
 import deepalayaLogoUrl from "./images/partners/deepalaya-logo.jpg"
@@ -18,10 +19,6 @@ const partners = [
     imageUrl: gujaratSealUrl,
   },
   {
-    name: "Sahyog Charitable Trust",
-    imageUrl: sahyogLogo,
-  },
-  {
     name: "Deepalaya",
     imageUrl: deepalayaLogoUrl,
   },
@@ -33,46 +30,48 @@ const partners = [
     name: "Sparsha Trust",
     imageUrl: sparshaTrustLogoUrl,
   },
+  {
+    name: "Sahyog Charitable Trust",
+    imageUrl: sahyogLogo,
+  },
 ]
 
-const OurPartnersSection = (props: BaseSectionProps) => (
-  <SectionContainer flex="grow" background="white">
-    <Box fill pad={{ horizontal: "40px" }} style={{ maxWidth: 1600 }} alignSelf="center">
-      <Box direction="column" fill="horizontal" align="start">
-        <Heading level={2} margin="0">
-          Our Partners
-        </Heading>
+const OurPartnersSection = (props: BaseSectionProps) => {
+  const [ref] = useKeenSlider<HTMLDivElement>({
+    slidesPerView: 2,
+    loop: true,
+  })
+
+  return (
+    <SectionContainer flex="grow" background="white">
+      <Box fill pad={{ horizontal: "40px" }} style={{ maxWidth: 1600 }} alignSelf="center">
+        <Box direction="column" fill="horizontal" align="start">
+          <Heading level={2} margin="0">
+            Our Partners
+          </Heading>
+        </Box>
+        <Box height="20px" />
+        <Box height="300px">
+          <div ref={ref} className="keen-slider">
+            {partners.map(({ imageUrl, name }) => (
+              <div className="keen-slider__slide" key={name}>
+                <Box justify="center" align="center" fill>
+                  <Box direction="column" pad="medium">
+                    <Image src={imageUrl} fit="contain" />
+                    <Box>
+                      <Text textAlign="center" size="large">
+                        {name}
+                      </Text>
+                    </Box>
+                  </Box>
+                </Box>
+              </div>
+            ))}
+          </div>
+        </Box>
       </Box>
-      <Box height="20px" />
-      <Box
-        style={{
-          display: "grid",
-          gridTemplateColumns: props.isMobileLayout ? "1fr 1fr" : "1fr 1fr 1fr",
-          rowGap: 40,
-          columnGap: 80,
-        }}
-      >
-        {partners.map(({ name, imageUrl }) => (
-          <Box
-            direction="column"
-            justify="end"
-            gap="medium"
-            flex="grow"
-            style={{
-              maxWidth: 250,
-            }}
-          >
-            <Image src={imageUrl} />
-            <Box>
-              <Text textAlign="center" size="large">
-                {name}
-              </Text>
-            </Box>
-          </Box>
-        ))}
-      </Box>
-    </Box>
-  </SectionContainer>
-)
+    </SectionContainer>
+  )
+}
 
 export default OurPartnersSection
