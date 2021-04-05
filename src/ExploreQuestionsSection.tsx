057ebbbe0ab5fba7b9d565c14590dbf09b91c781
Nick Artman <nicolasartman@gmail.com>
2021-04-05 00:57:29 -0700

    Add full-screen view of student answers to explore questions section
    The question items in the carousel can now be clicked for a full-screen
    modal that showcases the student's work!

--- src/ExploreQuestionsSection.tsx
+++ src/ExploreQuestionsSection.tsx
@@ -1,9 +1,21 @@
-import { Box, Heading, Image, ResponsiveContext } from "grommet"
+import { Box, Button, Heading, Image, Layer, ResponsiveContext, Text } from "grommet"
+import { Close as CloseIcon } from "grommet-icons"
 import "keen-slider/keen-slider.min.css"
 import { useKeenSlider } from "keen-slider/react"
 import { desaturate, transparentize } from "polished"
+import { useState } from "react"
 import { BaseSectionProps } from "./BaseSectionProps"
-import { cyan, darkGreen, darkPurple, darkRed, darkYellow, purple, red, white } from "./colors"
+import {
+  almostWhite,
+  cyan,
+  darkGreen,
+  darkPurple,
+  darkRed,
+  darkYellow,
+  purple,
+  red,
+  white,
+} from "./colors"
 import { questions } from "./images"
 import SectionContainer from "./SectionContainer"
 
@@ -12,11 +24,12 @@ type QuestionCardData = {
   question: string
   studentName: string
   grade?: number
-  image: string
+  imageUrl: string
 }
 
 type QuestionCardProps = QuestionCardData & {
   backgroundColor: string
+  onClick: React.PointerEventHandler
 }
 
 const colors = [red, purple, darkYellow, darkPurple, darkRed, cyan, darkGreen]
@@ -27,192 +40,192 @@ const cards: Array<QuestionCardData> = [
     question: "Why don't satellites fall out of the sky?",
     studentName: "Aditya",
     grade: 8,
-    image: questions.satellites,
+    imageUrl: questions.satellites,
   },
   {
     priority: 1,
     question: "Why do fruits change their color?",
     studentName: "Nigaaz Nashreen",
     grade: 6,
-    image: questions.fruits,
+    imageUrl: questions.fruits,
   },
   {
     priority: 1,
     question: "Which country first gave women the right to vote?",
     studentName: "Nandini Vashisht",
     grade: 10,
-    image: questions.womensVotingRights,
+    imageUrl: questions.womensVotingRights,
   },
   {
     priority: 1,
     question: "What is the reason behind an earthquake?",
     studentName: "Akash",
     grade: 7,
-    image: questions.earthquake,
+    imageUrl: questions.earthquake,
   },
   {
     priority: 1,
     question: "Why does an air conditioner drip water?",
     studentName: "Malek Abrar Ishakbhai",
     grade: 7,
-    image: questions.airConditioner,
+    imageUrl: questions.airConditioner,
   },
   {
     priority: 2,
     question: "How can I differentiate between an adjective and an adverb?",
     studentName: "Kirti",
-    image: questions.adjectiveVsAdverb,
+    imageUrl: questions.adjectiveVsAdverb,
   },
   {
     priority: 2,
     question: "Why does an eagle stop flapping its wings after reaching some height?",
     studentName: "Arti",
     grade: 7,
-    image: questions.eagleFlyingPatterns,
+    imageUrl: questions.eagleFlyingPatterns,
   },
   {
     priority: 2,
     question: "Who was Napoleon and what did he do?",
     studentName: "Altaf",
     grade: 10,
-    image: questions.napoleon,
+    imageUrl: questions.napoleon,
   },
   {
     priority: 2,
     question: "What is global warming?",
     studentName: "Hetal Girishbhai",
     grade: 8,
-    image: questions.globalWarming,
+    imageUrl: questions.globalWarming,
   },
   {
     priority: 2,
     question: "What are the uses of a cylinder?",
     studentName: "Aman",
     grade: 8,
-    image: questions.cylinder,
+    imageUrl: questions.cylinder,
   },
   {
     priority: 2,
     question: "Snakes don't have any ears. How are they able to hear?",
     studentName: "Anjuman Banu",
     grade: 7,
-    image: questions.snakesHearing,
+    imageUrl: questions.snakesHearing,
   },
   {
     priority: 2,
     question: "Why does the moon look half?",
     studentName: "Saiba",
     grade: 7,
-    image: questions.moonHalf,
+    imageUrl: questions.moonHalf,
   },
   {
     priority: 2,
     question: "Why is mango called the king of fruits?",
     studentName: "Jatin",
     grade: 5,
-    image: questions.mango,
+    imageUrl: questions.mango,
   },
   {
     priority: 2,
     question: "Why do they paint a giant X behind trains?",
     studentName: "Sadiya Pramod",
     grade: 9,
-    image: questions.trainX,
+    imageUrl: questions.trainX,
   },
   {
     priority: 2,
     question: "Why is the value of pi 22 by 7?",
     studentName: "Vishal",
     grade: 8,
-    image: questions.pi,
+    imageUrl: questions.pi,
   },
   {
     priority: 2,
     question: 'Why is "an" used before the word "hour"?',
     studentName: "Dhoni",
     grade: 7,
-    image: questions.articleForTheWordHour,
+    imageUrl: questions.articleForTheWordHour,
   },
   {
     priority: 2,
     question: "How did dinosaurs become extinct?",
     studentName: "Bhumika",
-    image: questions.dinosaurs,
+    imageUrl: questions.dinosaurs,
   },
   {
     priority: 2,
     question: "Why does owl appear at night and not in day?",
     studentName: "Shekh Alafina",
     grade: 5,
-    image: questions.owl,
+    imageUrl: questions.owl,
   },
   {
     priority: 2,
     question: "What is happiness?",
     studentName: "Anurag",
     grade: 9,
-    image: questions.happiness,
+    imageUrl: questions.happiness,
   },
   {
     priority: 2,
     question: "What would happen if there were no moon?",
     studentName: "Kavita Sahni Sheikh",
     grade: 8,
-    image: questions.noMoon,
+    imageUrl: questions.noMoon,
   },
   {
     priority: 2,
     question: "Why doesn't the moon fall to the earth?",
     studentName: "Pooja Chaurasiya",
-    image: questions.moonFallingToTheEarth,
+    imageUrl: questions.moonFallingToTheEarth,
   },
   {
     priority: 3,
     question: "Why do some people have six fingers?",
     studentName: "Sadiya Khan",
     grade: 10,
-    image: questions.sixFingers,
+    imageUrl: questions.sixFingers,
   },
   {
     priority: 3,
     question: "Why do I feel scared in the dark?",
     studentName: "Siddiki Farhan",
     grade: 9,
-    image: questions.scaredInTheDark,
+    imageUrl: questions.scaredInTheDark,
   },
   {
     priority: 3,
     question: "How is washing detergent made?",
     studentName: "Simran",
     grade: 8,
-    image: questions.washingDetergent,
+    imageUrl: questions.washingDetergent,
   },
   {
     priority: 3,
     question: "How does a chameleon change its color?",
     studentName: "Solanki",
     grade: 10,
-    image: questions.chameleon,
+    imageUrl: questions.chameleon,
   },
   {
     priority: 3,
     question: "What would happen if there were no snakes in the food chain?",
     studentName: "Sarla Saket",
     grade: 5,
-    image: questions.snakesInTheFoodChain,
+    imageUrl: questions.snakesInTheFoodChain,
   },
   {
     priority: 3,
     question: "How does water disappear?",
     studentName: "Hiren Muchadia",
     grade: 9,
-    image: questions.waterDisappear,
+    imageUrl: questions.waterDisappear,
   },
   {
     priority: 3,
     question: "Why does the sky look red in the morning and evening?",
     studentName: "Pallavi Muchadia",
-    image: questions.redSky,
+    imageUrl: questions.redSky,
   },
   // {
   //   priority: 3,
@@ -237,14 +250,12 @@ const QuestionCard = (props: QuestionCardProps) => {
     <ResponsiveContext.Consumer>
       {(size) => (
         <Box
-          style={{
-            position: "relative",
-            paddingBottom: `${ratio * 100}%`,
-            height: 0,
-          }}
+          style={{ position: "relative", paddingBottom: `${ratio * 100}%`, height: 0 }}
+          role="button"
+          onClick={props.onClick}
         >
           <Box style={{ position: "absolute", height: "100%", width: "100%" }}>
-            <Image fit="cover" src={props.image} fill={false} />
+            <Image fit="cover" src={props.imageUrl} fill={false} />
           </Box>
           <Box style={{ position: "absolute", top: 0, left: 0 }} fill>
             <Box direction="column" align="center" justify="center" fill>
@@ -298,6 +309,7 @@ type ExploreQuestionsSectionProps = BaseSectionProps & {
 
 const ExploreQuestionsSection = (props: ExploreQuestionsSectionProps) => {
   const slidesToShow = props.size === "small" ? 1.5 : props.size === "mediumSmall" ? 2.2 : 3.5
+  const [openedCard, setOpenedCard] = useState<QuestionCardData | null>(null)
   const [ref] = useKeenSlider<HTMLDivElement>({
     slidesPerView: slidesToShow,
     mode: "free-snap",
@@ -313,7 +325,11 @@ const ExploreQuestionsSection = (props: ExploreQuestionsSectionProps) => {
         <div ref={ref} className="keen-slider">
           {cards.map((card, index) => (
             <div className="keen-slider__slide" key={card.question}>
-              <QuestionCard {...card} backgroundColor={colors[index % colors.length]} />
+              <QuestionCard
+                {...card}
+                backgroundColor={colors[index % colors.length]}
+                onClick={() => setOpenedCard(card)}
+              />
             </div>
           ))}
         </div>
@@ -333,6 +349,61 @@ const ExploreQuestionsSection = (props: ExploreQuestionsSectionProps) => {
           </Heading>
         </Box>
       </Box>
+      {openedCard ? (
+        <Layer
+          onEsc={() => setOpenedCard(null)}
+          onClickOutside={() => setOpenedCard(null)}
+          animation="fadeIn"
+          background="rgba(0, 0, 0, 0.75)"
+          responsive
+          full
+        >
+          <Box fill style={{ position: "relative" }}>
+            {/* Background image */}
+            <Image fit="cover" src={openedCard.imageUrl} />
+
+            {/* Details */}
+            <Box
+              pad="medium"
+              background="rgba(0, 0, 0, 0.5)"
+              style={{ position: "absolute", bottom: 0, left: 0, width: "100%" }}
+            >
+              <Box>
+                <Heading fill level="2" textAlign="center" color={almostWhite}>
+                  {openedCard.question}
+                </Heading>
+                <Text textAlign="center">
+                  {openedCard.studentName}, Grade {openedCard.grade}
+                </Text>
+              </Box>
+            </Box>
+
+            {/* Close button */}
+            <Box
+              style={{
+                position: "absolute",
+                top: 10,
+                right: 10,
+              }}
+            >
+              <Button
+                plain
+                style={{
+                  background: "black",
+                  border: "2px solid white",
+                  borderRadius: "50%",
+                  padding: "20px",
+                  boxShadow: "none",
+                  opacity: 0.5,
+                }}
+                onClick={() => setOpenedCard(null)}
+              >
+                <CloseIcon color="white" size="medium" style={{ display: "block" }} />
+              </Button>
+            </Box>
+          </Box>
+        </Layer>
+      ) : null}
     </SectionContainer>
   )
 }
